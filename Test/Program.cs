30ff440cd9aa5221646aed21b47ab41bffe7999c
trinhquocphong.vnag@gmail.com <trinhquocphong.vnag@gmail.com>
2021-08-19 22:32:35 +0700

    end

--- Test/Program.cs
+++ Test/Program.cs
@@ -8,92 +8,104 @@ namespace test1
 {
     class Program
     {
+        static void importValue(out double firstNum, out double secondNum, out char operation)
+        {
+            Console.WriteLine("Please enter the first number: ");
+            while (true)
+            {
+                if (Double.TryParse(Console.ReadLine(), out firstNum) == true)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid number,   try enter the first number again: ");
+                }
+            }
+            Console.WriteLine("Please enter the second number: ");
+            while (true)
+            {
+                if (Double.TryParse(Console.ReadLine(), out secondNum) == true)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid number,   try enter the second number again: ");
+                }
+            }
+            Console.WriteLine("Press '+' to sum : ");
+            Console.WriteLine("Press '-' to subtract : ");
+            Console.WriteLine("Press '*' to multiply : ");
+            Console.WriteLine("Press '/' to divide : ");
+            Console.WriteLine("Please enter the operation: ");
+            while (true)
+            {
+                if (Char.TryParse(Console.ReadLine(), out operation) == true)
+                {
+                    if ((operation != '-') && (operation != '*') && (operation != '/') && (operation != '+'))
+                    {
+                        Console.WriteLine("Please try to enter the operation again: ");
+                    }
+                    else break;
+                }
+                else
+                    Console.WriteLine("Please try to enter the operation again: ");
+            }
+        }
+        static void calculate(char operation, double firstNum, double secondNum)
+        {
+            switch (operation)
+            {
+                case '+':
+                    {
+                        Console.Write(firstNum + secondNum);
+                        break;
+                    }
+                case '-':
+                    {
+                        Console.Write(firstNum - secondNum);
+                        break;
+                    }
+                case '*':
+                    {
+                        Console.Write(Math.Round(firstNum * secondNum, 2));
+                        break;
+                    }
+                case '/':
+                    {
+                        if (secondNum == 0) Console.WriteLine("Invalid calculation");
+                        else
+                            Console.Write(Math.Round(firstNum / secondNum, 2));
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Invalid operation");
+                        break;
+                    }
+            }
+        }
         static void Main(string[] args)
         {
             char operation;
-            double num1;
-            double num2;
+            double firstNum;
+            double secondNum;
             while (true)
             {
-                Console.WriteLine("Please enter the first number: ");
-                while (true)
-                {
-                    if (Double.TryParse(Console.ReadLine(), out num1) == true)
-                    {
-                        break;
-                    }
-                    else
-                    {
-                        Console.WriteLine("Invalid number,   try enter the first number again: ");
-                    }
-                }
-                Console.WriteLine("Please enter the second number: ");
-                while (true)
-                {
-                    if (Double.TryParse(Console.ReadLine(), out num2) == true)
-                    {
-                        break;
-                    }
-                    else
-                    {
-                        Console.WriteLine("Invalid number,   try enter the second number again: ");
-                    }
-                }
-                Console.WriteLine("Please enter the operation: ");
-                while(true)
-                {
-                    if (Char.TryParse(Console.ReadLine(), out operation) == true)
-                    {
-                        if ((operation != '-') && (operation != '*') && (operation != '/') && (operation != '+'))
-                        {
-                            Console.WriteLine("Please try to enter the operation again: ");
-                        }
-                        else break;
-                    }
-                    else
-                        Console.WriteLine("Please try to enter the operation again: ");
-                }
+                importValue(out firstNum, out secondNum, out operation);
                 Console.Clear();
-                Console.WriteLine("The first number is:   " + num1);
-                Console.WriteLine("The second number is:   " + num2);
+                Console.WriteLine("The first number is:   " + firstNum);
+                Console.WriteLine("The second number is:   " + secondNum);
                 Console.WriteLine("Result is: ");
-                switch (operation)
-                {
-                    case '+':
-                        {
-                            Console.Write(num1 + num2);
-                            break;
-                        }
-                    case '-':
-                        {
-                            Console.Write(num1 - num2);
-                            break;
-                        }
-                    case '*':
-                        {
-                            Console.Write(Math.Round(num1 * num2, 2));
-                            break;
-                        }
-                    case '/':
-                        {
-                            if (num2 == 0) Console.WriteLine("Invalid calculation");
-                            else
-                                Console.Write(Math.Round(num1 / num2, 2));
-                            break;
-                        }
-                    default:
-                        {
-                            Console.WriteLine("Invalid operation");
-                            break;
-                        }
-                }
+                calculate(operation, firstNum, secondNum);
                 Console.WriteLine("\n\n\tDo you want to restart?    Press 'n' to restart");
                 char n = char.Parse(Console.ReadLine());
                 if (n == 'n')
                 {
                     operation = ' ';
-                    num1 = 0;
-                    num2 = 0;
+                    firstNum = 0;
+                    secondNum = 0;
                     Console.Clear();
                 }
                 else
