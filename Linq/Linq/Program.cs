87069994c411f57a588e53a104fa3e72c442172c
trinhquocphong.vnag@gmail.com <trinhquocphong.vnag@gmail.com>
2021-08-20 22:39:09 +0700

    end

--- Linq/Linq/Program.cs
+++ Linq/Linq/Program.cs
@@ -1,12 +1,42 @@
 ﻿using System;
-
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 namespace Linq
 {
-    class Program
+   public static class linq
     {
+        public static IEnumerable<T> myOfType<T>(this IEnumerable list)
+        {
+            foreach (object type in list) if (type is T)
+                yield return (T)type;
+        }
+    }
+
+    class Program
+    {       
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("My LINQ!");
+            ArrayList data = new ArrayList();
+            data.Add("Nike");
+            data.Add("Adidas");
+            data.Add(4);
+            data.Add(19.5);
+            Console.WriteLine("\n\t\tArray before use my LINQ (OfType<string>)\n");
+            foreach (object item in data)
+            {
+                Console.Write( "    "+item );
+            }
+            Console.WriteLine("\n\n\n\n\t\tAnd this is our array after use my LINQ (OfType<string>)\n");
+            var list =data.myOfType<string>();
+            foreach (object item in list)
+            {
+                Console.Write("    " + item);
+            }
+            Console.ReadKey();
         }
     }
 }
