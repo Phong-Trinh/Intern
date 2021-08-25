20ff5c7b4627443deab967137d0bcffb3a921105
trinhquocphong.vnag@gmail.com <trinhquocphong.vnag@gmail.com>
2021-08-25 14:59:16 +0700

    da sua theo gop y cua Lam

--- fibonacci/fibonacci/Program.cs
+++ fibonacci/fibonacci/Program.cs
@@ -2,57 +2,85 @@
 
 namespace ConsoleApp2
 {
-    class Program
+    public abstract class Fibonacci
     {
-        static int fiboCallBack(int i)
+        public abstract int FigureOut(int i);
+    }
+    public class Way1 : Fibonacci
+    {
+        public override int FigureOut(int i)
         {
             if (i == 2 || i == 1) return 1;
-            else return fiboCallBack(i - 1) + fiboCallBack(i - 2);
+            else return FigureOut(i - 1) + FigureOut(i - 2);
         }
-        static void figureOut(int numFibo, char waySolve)
+    }
+    public class Way2 : Fibonacci
+    {
+        public override int FigureOut(int numFibo)
         {
             int fibo = 1;
             int temporary = 1;
+            int temporary1;
+            for (int i = 1; i <= numFibo; i++)
+            {
+                if (i == 2 || i == 1) fibo = 1;
+                else
+                {
+                    temporary1 = fibo;
+                    fibo += temporary;
+                    temporary = temporary1;
+                }
+            }
+            return fibo;
+        }
+    }
+    public class Way3 : Fibonacci
+    {
+        public override int FigureOut(int numFibo)
+        {
+            int fibo = 1;
+            int temporary = 1;
+            int[] fiboArr = new int[numFibo];
+            fiboArr[0] = 1;
+            if (numFibo >= 2)
+            {
+                fiboArr[1] = 1;
+                if (numFibo > 2)
+                    for (int i = 2; i < numFibo; i++)
+                    {
+                        fiboArr[i] = fiboArr[i - 1] + fiboArr[i - 2];
+                    }
+            }
+            fibo = fiboArr[numFibo - 1];
+            return fibo;
+        }
+    }
+    class Program
+    {
+        static void figureOut(int numFibo, char waySolve)
+        {
             switch (waySolve)
             {
                 case '1':
                     {
-                        int temporary1;
-                        for (int i = 1; i <= numFibo; i++)
-                        {
-                            if (i == 2 || i == 1) fibo = 1;
-                            else
-                            {
-                                temporary1 = fibo;
-                                fibo += temporary;
-                                temporary = temporary1;
-                            }
-                        }
+                        Fibonacci way1 = new Way1();
+                        numFibo =way1.FigureOut(numFibo);
                         break;
                     }
                 case '2':
                     {
-                        int[] fiboArr = new int[numFibo];
-                        fiboArr[0] = 1;
-                        if (numFibo >= 2)
-                        {
-                            fiboArr[1] = 1;
-                            if(numFibo > 2)
-                            for (int i = 2; i < numFibo; i++)
-                            {
-                                    fiboArr[i] = fiboArr[i - 1] + fiboArr[i - 2];
-                            }
-                        }
-                        fibo = fiboArr[numFibo - 1];
+                        Fibonacci way2 = new Way2();
+                        numFibo = way2.FigureOut(numFibo);
                         break;
                     }
                 case '3':
                     {
-                        fibo = fiboCallBack(numFibo);
+                        Fibonacci way3 = new Way3();
+                        numFibo = way3.FigureOut(numFibo);
                         break;
                     }
             }
-            Console.WriteLine(fibo);
+            Console.WriteLine(numFibo);
         }
         static void Main(string[] args)
         {
