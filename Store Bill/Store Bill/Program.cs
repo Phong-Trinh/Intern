9868df5d51f753ed52e20de62bdbfdd6c9b8d4ef
trinhquocphong.vnag@gmail.com <trinhquocphong.vnag@gmail.com>
2021-08-26 19:28:44 +0700

    begin

---
+++ Store Bill/Store Bill/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Store_Bill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
